80e1528ddd57a130c84ec4d94d01c3785b4657ac
vigneshm04 <vignesh21221@gmail.com>
2022-01-10 12:33:03 +0530

    Successfully completed

---
+++ src/org/git/First.java
@@ -0,0 +1,11 @@
+package org.git;
+
+public class First {
+	private void one() {
+		System.out.println("one");
+	}
+public static void main(String[] args) {
+	First f = new First();
+	f.one();
+}
+}
